7b30f17b7e4409ceadb51a24f88f3b6ee6c47d0c
august <august1903.ap@gmail.com>
2025-07-12 03:24:04 +0700

    make switch scene

---
+++ Script/title_screen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_startbtn_pressed():
+	get_tree().change_scene_to_file("res://Asset/in_game_screen.tscn")
+	pass # Replace with function body.
+
+
+func _on_quitbtn_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
