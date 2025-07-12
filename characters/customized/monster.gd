dec79aa9a02dcfb5ba96bf0643dfdb7a11cae7f5
Boby <boblaveyou@gmail.com>
2025-07-13 04:49:08 +0700

    chromastome health

--- characters/customized/monster.gd
+++ characters/customized/monster.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var player
+var health = 100
 
 func _ready() -> void:
 	player = get_node("../player")
@@ -14,3 +15,17 @@ func _physics_process(delta):
 		get_node("chroma").play_walk()
 	else :
 		get_node("chroma").play_idle()
+
+func take_damage(amount: int = 1):
+	health -= amount
+	if health <= 0:
+		die()
+		
+func die():
+	# Play death animation if you have one
+	# If you have a death animation, uncomment the lines below:
+	# if has_node("mob") and get_node("mob").has_method("play_death_animation"):
+	#     get_node("mob").play_death_animation()
+	#     await get_tree().create_timer(0.5).timeout  # Wait for animation
+	
+	queue_free()
